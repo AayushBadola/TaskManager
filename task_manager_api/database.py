d786584aa03427e62d640130212f8cdd1d1d6125
Aayush Badola <aayush.badola2@gmail.com>
2025-04-29 17:40:38 +0530

    Update database.py

--- task_manager_api/database.py
+++ task_manager_api/database.py
@@ -1,1 +1,23 @@
 
+from typing import Dict, Generator
+from .models import Task
+
+fake_db: Dict[int, Task] = {}
+_next_id: int = 1
+
+def get_db() -> Generator[Dict[int, Task], None, None]:
+    try:
+        yield fake_db
+    finally:
+        pass
+
+def get_next_id() -> int:
+    global _next_id
+    current_id = _next_id
+    _next_id += 1
+    return current_id
+
+def reset_db_for_testing() -> None:
+    global fake_db, _next_id
+    fake_db = {}
+    _next_id = 1
