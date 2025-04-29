fba26e8e569bd4e61093b6f035588b862fdc2476
Aayush Badola <aayush.badola2@gmail.com>
2025-04-29 17:41:59 +0530

    Update models.py

--- task_manager_api/models.py
+++ task_manager_api/models.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Task(BaseModel):
+    id: int
+    title: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(None, max_length=500)
+    completed: bool = False
 
