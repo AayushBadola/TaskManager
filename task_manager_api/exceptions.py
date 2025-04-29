be09fe8d03295560717570a06227397f37b2a76c
Aayush Badola <aayush.badola2@gmail.com>
2025-04-29 17:40:56 +0530

    Update exceptions.py

--- task_manager_api/exceptions.py
+++ task_manager_api/exceptions.py
@@ -1,1 +1,10 @@
 
+class TaskNotFoundException(Exception):
+    def __init__(self, task_id: int):
+        self.task_id = task_id
+        super().__init__(f"Task with id {task_id} not found")
+
+class DatabaseErrorException(Exception):
+    def __init__(self, message: str = "A database error occurred"):
+        self.message = message
+        super().__init__(message)
